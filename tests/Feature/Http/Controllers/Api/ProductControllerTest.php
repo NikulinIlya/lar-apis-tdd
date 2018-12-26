e457d32021d769903f2dd5d7a9214743d5c79253
NikulinIlya <ilia.nikulin@gmail.com>
2018-12-26 20:05:16 +0300

    test(database): add test for making data in products table

--- tests/Feature/Http/Controllers/Api/ProductControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ProductControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
+use Faker\Factory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -17,14 +18,26 @@ class ProductControllerTest extends TestCase
     {
         //Given
             // user is authenticated
+
         //When
             // post request create product
-        $response = $this->json('POST', '/api/products', [
+        $faker = Factory::create();
 
+        $response = $this->json('POST', '/api/products', [
+            'name' => $name = $faker->company,
+            'slug' => str_slug($name),
+            'price' => $price = random_int(10, 100)
         ]);
+
         //Then
             // product exists
         $response->assertStatus(201);
+
+        $this->assertDatabaseHas('products', [
+            'name' => $name,
+            'slug' => str_slug($name),
+            'price' => $price
+        ]);
 
     }
 }
